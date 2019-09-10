f88adfd5cad80e01351f9d18ba919400c9f195b8
Camilo Roca <camilo.roca@cobi.bike>
2019-09-10 23:12:54 +0200

    fix eslint issues gitignore updated

--- src/cli.js
+++ src/cli.js
@@ -1,26 +1,23 @@
 #!/usr/bin/env node
 
-const path = require('path')
-const fs = require('fs')
-const {docopt, DocoptLanguageError, DocoptExit} = require('docopt')
-const hugsql = require('./compiler')
-const {version, description} = require('../package.json')
+const path = require('path');
+const fs = require('fs');
+const { docopt, DocoptLanguageError, DocoptExit } = require('docopt');
+const hugsql = require('./compiler');
+const { version, description } = require('../package.json');
 
 /**
  * Synchronously list all files in a directory recursively
  * @param {String} dir a fullpath directory
  */
 function* recursiveReaddirSync(dir) {
-    for(const child of fs.readdirSync(dir, {withFileTypes: true})) {
-
-        if (child.isDirectory()) {
-            yield * recursiveReaddirSync(path.resolve(dir, child.name))
-        }
-        // file otherwise
-        else {
-            yield path.resolve(dir, child.name)
-        }
+  for (const child of fs.readdirSync(dir, { withFileTypes: true })) {
+    if (child.isDirectory()) {
+      yield* recursiveReaddirSync(path.resolve(dir, child.name));
+    } else { // file otherwise
+      yield path.resolve(dir, child.name);
     }
+  }
 }
 
 const docstring = `
@@ -38,33 +35,33 @@ Options:
 Examples:
     hugsql ./resources
     hugsql ./resources --labeled
-`
-try {
-    // parse user arguments
-    const options = docopt(docstring, {
-        version,
-        help: true,
-        exit: false
-    })
-    // find all files in the provided dir
-    const files = recursiveReaddirSync(path.resolve(options['<dirpath>']))
-// compile a js file for each sql file found
-    for (const filepath of files) {
-        if(path.extname(filepath) === '.sql') {
-            const output = hugsql.compile(filepath, options['--labeled'])
-            const filename = path.basename(filepath, '.sql')
-            const filedir = path.dirname(filepath)
-            fs.writeFileSync(path.join(filedir, `${filename}.sql.js`), output)
-        }
-    }
+`;
 
-} catch (error) {
-    // error thrown by docopt or
-    if (error instanceof DocoptLanguageError || error instanceof DocoptExit) {
-        console.error(`Error parsing user arguments. Please check the usage` +
-            ` section below and try again.\n\n${docstring}`)
-    } else {
-        console.error(error.toString())
+try {
+  // parse user arguments
+  const options = docopt(docstring, {
+    version,
+    help: true,
+    exit: false,
+  });
+  // find all files in the provided dir
+  const files = recursiveReaddirSync(path.resolve(options['<dirpath>']));
+  // compile a js file for each sql file found
+  for (const filepath of files) {
+    if (path.extname(filepath) === '.sql') {
+      const output = hugsql.compile(filepath, options['--labeled']);
+      const filename = path.basename(filepath, '.sql');
+      const filedir = path.dirname(filepath);
+      fs.writeFileSync(path.join(filedir, `${filename}.sql.js`), output);
     }
-    process.exit(1)
+  }
+} catch (error) {
+  // error thrown by docopt or
+  if (error instanceof DocoptLanguageError || error instanceof DocoptExit) {
+    console.error('Error parsing user arguments. Please check the usage'
+            + ` section below and try again.\n\n${docstring}`);
+  } else {
+    console.error(error.toString());
+  }
+  process.exit(1);
 }
--- src/compiler.js
+++ src/compiler.js
@@ -1,15 +1,15 @@
 
-const fs = require('fs')
-const Mustache = require('mustache')
+const fs = require('fs');
+const Mustache = require('mustache');
 
-const namelessTemplate = fs.readFileSync(`${__dirname}/../resources/nameless.mustache`, 'utf8')
-const labeledTemplate = fs.readFileSync(`${__dirname}/../resources/labeled.mustache`, 'utf8')
+const namelessTemplate = fs.readFileSync(`${__dirname}/../resources/nameless.mustache`, 'utf8');
+const labeledTemplate = fs.readFileSync(`${__dirname}/../resources/labeled.mustache`, 'utf8');
 
-const sectionRegex = /(\/\*\*.*?\*\/)\n*(.*?;)/isg
-const jsDocFunctionRegex = /@function (\w+)/
+const sectionRegex = /(\/\*\*.*?\*\/)\n*(.*?;)/isg;
+const jsDocFunctionRegex = /@function (\w+)/;
 // $ is a valid Javascript identifier
-const jsDocParamRegex = /@param ({\w+} )?(\$?\w+)(.*)/g
-const sqlParamRegex = / (:|@|\$)(\w+)/gi
+const jsDocParamRegex = /@param ({\w+} )?(\$?\w+)(.*)/g;
+const sqlParamRegex = / (:|@|\$)(\w+)/gi;
 
 /**
  * Returns a lazy sequence of regex executions over text
@@ -18,14 +18,14 @@ const sqlParamRegex = / (:|@|\$)(\w+)/gi
  * @yield {IterableIterator<Array<String>>}
  */
 function* allRegexMatches(text, regex) {
-    while(true) {
-        const section = regex.exec(text)
-        if (section === null) {
-            return
-        }
-
-        yield [...section]
+  while (true) {
+    const section = regex.exec(text);
+    if (section === null) {
+      return;
     }
+
+    yield [...section];
+  }
 }
 
 /**
@@ -35,13 +35,13 @@ function* allRegexMatches(text, regex) {
  * @return {Set<*>}
  */
 function difference(s1, s2) {
-    const result = new Set()
-    for (const value of s1) {
-        if (!s2.has(value)) {
-            result.add(value)
-        }
+  const result = new Set();
+  for (const value of s1) {
+    if (!s2.has(value)) {
+      result.add(value);
     }
-    return result
+  }
+  return result;
 }
 
 /**
@@ -50,17 +50,17 @@ function difference(s1, s2) {
  * @return {{query: string, sortedParameters: Array<String>}}
  */
 function anonymize(sqlStatement) {
-    let counter = 0
-    const sortedParameters = []
-    const anonymized = sqlStatement.replace(sqlParamRegex, function (match, symbol, name) {
-        sortedParameters[counter] = symbol === '$' ? match.trim() : name
-        counter = counter + 1
-        return ' ?'
-    })
-    return {
-        sortedParameters,
-        query: anonymized,
-    }
+  let counter = 0;
+  const sortedParameters = [];
+  const anonymized = sqlStatement.replace(sqlParamRegex, (match, symbol, name) => {
+    sortedParameters[counter] = symbol === '$' ? match.trim() : name;
+    counter += 1;
+    return ' ?';
+  });
+  return {
+    sortedParameters,
+    query: anonymized,
+  };
 }
 
 /**
@@ -70,28 +70,28 @@ function anonymize(sqlStatement) {
  * @return {Array<String>} the names of the parameters (without :$@ symbols)
  */
 function checkParameters(sqlStatement, jsDoc) {
-    // $ is a valid identifier in Javascript so if we find it accept it as part of the name
-    const sqlParameters = [...allRegexMatches(sqlStatement, sqlParamRegex)]
-        .map(([match, symbol, name]) => symbol === '$' ? match.trim() : name)
+  // $ is a valid identifier in Javascript so if we find it accept it as part of the name
+  const sqlParameters = [...allRegexMatches(sqlStatement, sqlParamRegex)]
+    .map(([match, symbol, name]) => (symbol === '$' ? match.trim() : name));
 
-    const jsDocParameters = [...allRegexMatches(jsDoc, jsDocParamRegex)]
-        .reduce((result, [text, type, name]) => [...result, name], [])
+  const jsDocParameters = [...allRegexMatches(jsDoc, jsDocParamRegex)]
+    .reduce((result, [, , name]) => [...result, name], []);
 
-    const sqlJsDifference = difference(new Set(sqlParameters), new Set(jsDocParameters))
-    if (sqlJsDifference.size !== 0) {
-        throw new SyntaxError(`"${[...sqlJsDifference]}" parameters` +
-            ` found in Sql statement:\n\n${sqlStatement}\n\nbut not in JsDoc` +
-            ` section:\n${jsDoc}`)
-    }
+  const sqlJsDifference = difference(new Set(sqlParameters), new Set(jsDocParameters));
+  if (sqlJsDifference.size !== 0) {
+    throw new SyntaxError(`"${[...sqlJsDifference]}" parameters`
+            + ` found in Sql statement:\n\n${sqlStatement}\n\nbut not in JsDoc`
+            + ` section:\n${jsDoc}`);
+  }
 
-    const jsSqlDifference = difference(new Set(jsDocParameters), new Set(sqlParameters))
-    if (jsSqlDifference.size !== 0) {
-        throw new SyntaxError(`"${[...jsSqlDifference]}" parameters` +
-            ` found in JsDoc section:\n\n${jsDoc}\n\nbut not in Sql` +
-            ` statement:\n${sqlStatement}`)
-    }
+  const jsSqlDifference = difference(new Set(jsDocParameters), new Set(sqlParameters));
+  if (jsSqlDifference.size !== 0) {
+    throw new SyntaxError(`"${[...jsSqlDifference]}" parameters`
+            + ` found in JsDoc section:\n\n${jsDoc}\n\nbut not in Sql`
+            + ` statement:\n${sqlStatement}`);
+  }
 
-    return jsDocParameters
+  return jsDocParameters;
 }
 
 /**
@@ -99,41 +99,45 @@ function checkParameters(sqlStatement, jsDoc) {
  * a sequence of objects that can be used for rendering with mustache
  * @param {String} fileContent
  * @param {Boolean} labeled
- * @yield {functionName: string, sortedParameters: string, docstring: string, query: string, parameters: Array<String>}
+ * @yield {functionName: string,
+ *         sortedParameters: string,
+ *         docstring: string,
+ *         query: string,
+ *         parameters: Array<String>}
  */
 function* parseContent(fileContent, labeled) {
-    for (const section of allRegexMatches(fileContent, sectionRegex)) {
-        const [, docstringBlock, rawSqlStatement] = section
+  for (const section of allRegexMatches(fileContent, sectionRegex)) {
+    const [, docstringBlock, rawSqlStatement] = section;
 
-        // extract basic info on the current section
-        const functionBlock = jsDocFunctionRegex.exec(docstringBlock)
-        if (functionBlock === null) {
-            throw new SyntaxError(`Missing @function in docstring` +
-                ` section ${docstringBlock}`)
-        }
-        const [jsDocLine, functionName] = functionBlock
-
-        // did the user forget to annotate anything ?
-        const parameters = checkParameters(rawSqlStatement, docstringBlock)
-
-        // normalize input data
-        const { query, sortedParameters } = labeled === false ? anonymize(rawSqlStatement) : {
-            query: rawSqlStatement
-        }
-
-        if (labeled === true && parameters.some((param) => !param.startsWith('$'))) {
-            throw new SyntaxError(`Only '$name' parameters are allowed on --labeled mode.` +
-             `\nPlease rename these parameters: ${parameters} on the statement:\n\n${rawSqlStatement}`)
-        }
-
-        yield {
-            query,
-            functionName,
-            parameters,
-            sortedParameters,
-            docstring: docstringBlock.replace(jsDocLine, ''),
-        }
+    // extract basic info on the current section
+    const functionBlock = jsDocFunctionRegex.exec(docstringBlock);
+    if (functionBlock === null) {
+      throw new SyntaxError('Missing @function in docstring'
+                + ` section ${docstringBlock}`);
     }
+    const [jsDocLine, functionName] = functionBlock;
+
+    // did the user forget to annotate anything ?
+    const parameters = checkParameters(rawSqlStatement, docstringBlock);
+
+    // normalize input data
+    const { query, sortedParameters } = labeled === false ? anonymize(rawSqlStatement) : {
+      query: rawSqlStatement,
+    };
+
+    if (labeled === true && parameters.some((param) => !param.startsWith('$'))) {
+      throw new SyntaxError('Only \'$name\' parameters are allowed on --labeled mode.'
+             + `\nPlease rename these parameters: ${parameters} on the statement:\n\n${rawSqlStatement}`);
+    }
+
+    yield {
+      query,
+      functionName,
+      parameters,
+      sortedParameters,
+      docstring: docstringBlock.replace(jsDocLine, ''),
+    };
+  }
 }
 
 /**
@@ -145,13 +149,12 @@ function* parseContent(fileContent, labeled) {
  *                          generated Js functions
  * @return {String} a Js file with functions containing the Sql statements
  */
-module.exports.compile = function (filepath, labeled) {
+module.exports.compile = function compile(filepath, labeled) {
+  const template = labeled === true ? labeledTemplate : namelessTemplate;
 
-    const template = labeled === true ? labeledTemplate : namelessTemplate
+  const fileContent = fs.readFileSync(filepath, 'utf8');
 
-    const fileContent = fs.readFileSync(filepath, 'utf8')
-
-    return Mustache.render(template, {
-        sections: [...parseContent(fileContent, labeled)]
-    })
-}
+  return Mustache.render(template, {
+    sections: [...parseContent(fileContent, labeled)],
+  });
+};
