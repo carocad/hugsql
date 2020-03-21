f431bd2dd0ed2f8607ff24670d75487368dc4c51
Camilo Roca <camilo.roca@cobi.bike>
2020-03-21 17:48:09 +0100

    sqlite.test.js modified to use the new template

--- test/sqlite.test.js
+++ test/sqlite.test.js
@@ -2,14 +2,8 @@
 const sqlite3 = require('sqlite3');
 
 const db = new sqlite3.Database(`${__dirname}/../resources/chinook.db`); // , sqlite3.OPEN_READONLY);
-const queries = require('./sql/chinook.sql.js');
+const statements = require('./sql/chinook.sql.js')(db);
 
-const calle13 = queries.findAllAlbums(1, 1);
-db.all(calle13.query, calle13.parameters, (error, rows) => {
-  if (error) {
-    console.error(error);
-  }
-  for (const row of rows) {
-    console.log(`rows: ${JSON.stringify(row)}`);
-  }
-});
+statements.findAllArtists()
+  .then(console.log)
+  .catch(console.error);
